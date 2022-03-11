4590bf9f312398e9fac95ae501cee3bf95bbfdb0
Bagabandi <76275588+Bagabandi152@users.noreply.github.com>
2022-03-11 10:39:35 +0800

    Update OrderActivity.java

--- Lab04/4.3/DroidCafeOptions/app/src/main/java/com/example/android/droidcafeoptions/OrderActivity.java
+++ Lab04/4.3/DroidCafeOptions/app/src/main/java/com/example/android/droidcafeoptions/OrderActivity.java
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2018 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.android.droidcafeoptions;
 
 import android.content.Intent;
@@ -24,18 +8,8 @@ import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
-/**
- * This activity handles radio buttons for choosing a delivery method for an
- * order, and EditText input controls.
- */
 public class OrderActivity extends AppCompatActivity {
 
-    /**
-     * Sets the content view to activity_order, and gets the intent and its
-     * data.
-     *
-     * @param savedInstanceState Saved instance state bundle.
-     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,12 +22,6 @@ public class OrderActivity extends AppCompatActivity {
         textView.setText(message);
     }
 
-    /**
-     * Checks which radio button was clicked and displays a toast message to
-     * show the choice.
-     *
-     * @param view The radio button view.
-     */
     public void onRadioButtonClicked(View view) {
         // Is the button now checked?
         boolean checked = ((RadioButton) view).isChecked();
@@ -80,11 +48,6 @@ public class OrderActivity extends AppCompatActivity {
         }
     }
 
-    /**
-     * Displays the actual message in a toast message.
-     *
-     * @param message Message to display.
-     */
     public void displayToast(String message) {
         Toast.makeText(getApplicationContext(), message,
                 Toast.LENGTH_SHORT).show();
