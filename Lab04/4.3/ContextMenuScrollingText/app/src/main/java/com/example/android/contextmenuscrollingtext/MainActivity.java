851e8e1e18278666e458c30ef2760010f8c67dc9
Bagabandi <76275588+Bagabandi152@users.noreply.github.com>
2022-03-11 10:37:52 +0800

    Update MainActivity.java

--- Lab04/4.3/ContextMenuScrollingText/app/src/main/java/com/example/android/contextmenuscrollingtext/MainActivity.java
+++ Lab04/4.3/ContextMenuScrollingText/app/src/main/java/com/example/android/contextmenuscrollingtext/MainActivity.java
@@ -1,23 +1,7 @@
-/*
- * Copyright (C) 2018 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.android.contextmenuscrollingtext;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.app.AppCompatActivity;
 import android.view.ContextMenu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -25,11 +9,6 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-/**
- * This app displays a scrollable TextView (a magazine article).
- * It includes a context menu with choices for acting on
- * the article (Edit, Share, Delete).
- */
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -42,13 +21,6 @@ public class MainActivity extends AppCompatActivity {
         registerForContextMenu(article_text);
     }
 
-    /**
-     * Create and inflate a context menu.
-     *
-     * @param menu The context menu to be built
-     * @param v The view to be accessed by the context menu
-     * @param menuInfo Extra information about the view
-     */
     @Override
     public void onCreateContextMenu(ContextMenu menu,
                                     View v,
@@ -58,13 +30,6 @@ public class MainActivity extends AppCompatActivity {
         inflater.inflate(R.menu.menu_context, menu);
     }
 
-    /**
-     * Handle the click of a contextual menu item.
-     *
-     * @param item The menu item that was selected
-     * @return false to allow normal context menu processing to proceed,
-     * true to consume it here.
-     */
     @Override
     public boolean onContextItemSelected(MenuItem item) {
         switch (item.getItemId()) {
@@ -82,11 +47,6 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    /**
-     * Displays a Toast with the message.
-     *
-     * @param message Message to display
-     */
     private void displayToast(String message) {
         Toast.makeText(getApplicationContext(), message,
                 Toast.LENGTH_SHORT).show();;
