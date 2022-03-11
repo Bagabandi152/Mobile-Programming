f94b2d47a492d4908b7823123f6ac8973835f407
Bagabandi <76275588+Bagabandi152@users.noreply.github.com>
2022-03-11 10:23:38 +0800

    Update WordListAdapter.java

--- Lab04/4.5/RecyclerView/app/src/main/java/com/example/android/recyclerview/WordListAdapter.java
+++ Lab04/4.5/RecyclerView/app/src/main/java/com/example/android/recyclerview/WordListAdapter.java
@@ -9,10 +9,6 @@ import android.widget.TextView;
 
 import java.util.LinkedList;
 
-/**
- * Shows how to implement a simple Adapter for a RecyclerView.
- * Demonstrates how to add a click handler for each item in the ViewHolder.
- */
 public class WordListAdapter extends
         RecyclerView.Adapter<WordListAdapter.WordViewHolder> {
 
@@ -24,14 +20,6 @@ public class WordListAdapter extends
         public final TextView wordItemView;
         final WordListAdapter mAdapter;
 
-        /**
-         * Creates a new custom view holder to hold the view to display in
-         * the RecyclerView.
-         *
-         * @param itemView The view in which to display the data.
-         * @param adapter The adapter that manages the the data and views
-         *                for the RecyclerView.
-         */
         public WordViewHolder(View itemView, WordListAdapter adapter) {
             super(itemView);
             wordItemView = itemView.findViewById(R.id.word);
@@ -59,26 +47,6 @@ public class WordListAdapter extends
         mInflater = LayoutInflater.from(context);
         this.mWordList = wordList;
     }
-
-    /**
-     * Called when RecyclerView needs a new ViewHolder of the given type to
-     * represent an item.
-     *
-     * This new ViewHolder should be constructed with a new View that can
-     * represent the items of the given type. You can either create a new View
-     * manually or inflate it from an XML layout file.
-     *
-     * The new ViewHolder will be used to display items of the adapter using
-     * onBindViewHolder(ViewHolder, int, List). Since it will be reused to
-     * display different items in the data set, it is a good idea to cache
-     * references to sub views of the View to avoid unnecessary findViewById()
-     * calls.
-     *
-     * @param parent   The ViewGroup into which the new View will be added after
-     *                 it is bound to an adapter position.
-     * @param viewType The view type of the new View. @return A new ViewHolder
-     *                 that holds a View of the given view type.
-     */
     @Override
     public WordListAdapter.WordViewHolder onCreateViewHolder(ViewGroup parent,
                                                              int viewType) {
@@ -88,16 +56,6 @@ public class WordListAdapter extends
         return new WordViewHolder(mItemView, this);
     }
 
-    /**
-     * Called by RecyclerView to display the data at the specified position.
-     * This method should update the contents of the ViewHolder.itemView to
-     * reflect the item at the given position.
-     *
-     * @param holder   The ViewHolder which should be updated to represent
-     *                 the contents of the item at the given position in the
-     *                 data set.
-     * @param position The position of the item within the adapter's data set.
-     */
     @Override
     public void onBindViewHolder(WordListAdapter.WordViewHolder holder,
                                  int position) {
@@ -107,11 +65,6 @@ public class WordListAdapter extends
         holder.wordItemView.setText(mCurrent);
     }
 
-    /**
-     * Returns the total number of items in the data set held by the adapter.
-     *
-     * @return The total number of items in this adapter.
-     */
     @Override
     public int getItemCount() {
         return mWordList.size();
