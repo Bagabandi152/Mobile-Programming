600b2436f56937ae2ecad48bed1d714f2f2b6904
Bagabandi <76275588+Bagabandi152@users.noreply.github.com>
2022-03-11 10:47:57 +0800

    Update PagerAdapter.java

--- Lab04/4.4/TabExperiment/app/src/main/java/com/example/android/tabexperiment/PagerAdapter.java
+++ Lab04/4.4/TabExperiment/app/src/main/java/com/example/android/tabexperiment/PagerAdapter.java
@@ -1,28 +1,10 @@
-/*
- * Copyright (C) 2018 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.android.tabexperiment;
 
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.app.Fragment;
+import android.support.app.FragmentManager;
+import android.support.app.FragmentStatePagerAdapter;
 
-/**
- * Fragment to return the clicked tab.
- */
+
 public class PagerAdapter extends FragmentStatePagerAdapter {
     int mNumOfTabs;
 
@@ -31,11 +13,6 @@ public class PagerAdapter extends FragmentStatePagerAdapter {
         this.mNumOfTabs = NumOfTabs;
     }
 
-    /**
-     * Return the Fragment associated with a specified position.
-     *
-     * @param position
-     */
     @Override
     public Fragment getItem(int position) {
         switch (position) {
@@ -46,9 +23,6 @@ public class PagerAdapter extends FragmentStatePagerAdapter {
         }
     }
 
-    /**
-     * Return the number of views available.
-     */
     @Override
     public int getCount() {
         return mNumOfTabs;
