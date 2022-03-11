6c95e5b9c24e2a13c1eff2eabaf619e3f48ff40e
Bagabandi <76275588+Bagabandi152@users.noreply.github.com>
2022-03-11 10:41:16 +0800

    Update DatePickerFragment.java

--- Lab04/4.3/PickerForDate/app/src/main/java/com/example/android/pickerfordate/DatePickerFragment.java
+++ Lab04/4.3/PickerForDate/app/src/main/java/com/example/android/pickerfordate/DatePickerFragment.java
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2018 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.android.pickerfordate;
 
 
@@ -27,18 +11,9 @@ import android.widget.DatePicker;
 
 import java.util.Calendar;
 
-/**
- * A simple {@link Fragment} subclass for the date picker.
- */
 public class DatePickerFragment extends DialogFragment
         implements DatePickerDialog.OnDateSetListener {
 
-    /**
-     * Creates the date picker dialog with the current date from Calendar.
-     *
-     * @param savedInstanceState    Saved instance state bundle
-     * @return DatePickerDialog     The date picker dialog
-     */
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
@@ -51,15 +26,7 @@ public class DatePickerFragment extends DialogFragment
         // Create a new instance of DatePickerDialog and return it.
         return new DatePickerDialog(getActivity(), this, year, month, day);
     }
-
-    /**
-     * Grabs the date and passes it to processDatePickerResult().
-     *
-     * @param datePicker  The date picker view
-     * @param year  The year chosen
-     * @param month The month chosen
-     * @param day   The day chosen
-     */
+    
     @Override
     public void onDateSet(DatePicker datePicker, int year, int month, int day) {
         // Set the activity to the Main Activity.
