eaf20e51970693d6e2bab6990b8bec8f7b4fe759
Bagabandi <76275588+Bagabandi152@users.noreply.github.com>
2022-03-11 10:39:47 +0800

    Update OrderActivity.java

--- Lab04/4.3/DroidCafeOptions/app/src/main/java/com/example/android/droidcafeoptions/OrderActivity.java
+++ Lab04/4.3/DroidCafeOptions/app/src/main/java/com/example/android/droidcafeoptions/OrderActivity.java
@@ -2,7 +2,7 @@ package com.example.android.droidcafeoptions;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.app.AppCompatActivity;
 import android.view.View;
 import android.widget.RadioButton;
 import android.widget.TextView;
